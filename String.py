eac1a3f2b08ac2976114b16da9587493dcd7b5cf
lykling <lykling.lyk@gmail.com>
2013-10-23 16:36:16 +0000

    add int2buf method

--- String.py
+++ String.py
@@ -13,6 +13,17 @@ def randstr(length):
     """
     return ''.join([chr(random.randint(32, 127)) for i in range(length)])
 
+def int2buf(src, length=1):
+    """
+    convert integer to buffer string
+    """
+    result = []
+    while length:
+        result.insert(0, chr(src & 0xff))
+        src = src >> 8
+        length -= 1
+    return ''.join(result)
+
 
 if __name__ == "__main__":
     print randstr(10);
